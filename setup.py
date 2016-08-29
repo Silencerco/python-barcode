cc0a06ef0fa5e78eea8c26c6463d0fb2b7715436
Pedro Salgado <steenzout@ymail.com>
2016-08-29 11:18:35 -0600

    updated setup.py.

--- setup.py
+++ setup.py
@@ -1,40 +1,53 @@
 # -*- coding: utf-8 -*-
 
-import sys
-from os.path import join, dirname
+import pip.download
 
-import barcode as pkg
+from pip.req import parse_requirements
 
 from setuptools import setup, find_packages
 
-
-# Avoid name clashes if the user has Python 2 and 3 installed
-console_script = 'pybarcode{0}'.format(sys.version_info[0])
-try:
-    import argparse  # lint:ok
-    required = []
-except ImportError:
-    required = ['argparse']
-
-with open(join(dirname(__file__), 'README.rst')) as fp:
-    long_desc = fp.read()
+exec(open('steenzout/barcode/version.py').read())
 
 
 setup(
-    name=pkg.__project__,
-    version=pkg.__release__,
-    packages=find_packages(),
-    url=pkg.__url__,
-    license=pkg.__license__,
-    author=pkg.__author__,
-    author_email=pkg.__author_email__,
-    description=pkg.__description__,
-    long_description=long_desc,
-    classifiers=pkg.__classifiers__,
-    entry_points={
-        'console_scripts':
-            ['{0} = barcode.pybarcode:main'.format(console_script)],
+    name='steenzout.barcode',
+    description='Python library for bar code generation.',
+    author='Thorsten Weimann, Alexander Shorin, Pedro Salgado',
+    author_email='kxepal@gmail.com,steenzout@ymail.com',
+    version=__version__,
+    maintainer='Pedro Salgado',
+    maintainer_email='steenzout@ymail.com',
+    url='https://github.com/steenzout/python-barcode/',
+    namespace_packages=['steenzout'],
+    packages=find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests')),
+    package_data={
+        '': [
+            'LICENSE', 'NOTICE.md', 'README.md'],
+        'steenzout.barcode': [
+            'steenzout/barcode/fonts/*']
     },
-    install_requires=required,
-    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Multimedia :: Graphics',
+    ],
+    install_requires=[
+        str(pkg.req) for pkg in parse_requirements(
+            'requirements.txt', session=pip.download.PipSession())],
+    tests_require=[
+        str(pkg.req) for pkg in parse_requirements(
+            'test-requirements.txt', session=pip.download.PipSession())],
+    license='MIT',
 )
