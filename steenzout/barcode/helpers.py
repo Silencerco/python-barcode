85ddfcec66c91a0909064fb07a6e5d5016936141
Pedro Salgado <steenzout@ymail.com>
2016-08-31 09:49:52 -0600

    renamed variables.

--- steenzout/barcode/helpers.py
+++ steenzout/barcode/helpers.py
@@ -2,14 +2,14 @@
 """Helpers module."""
 
 
-def sum_chars(x, y):
+def sum_chars(char, other):
     """Sum the value of two string characters.
 
     Args:
-        x (char): string of the integer.
-        y (char): string of the integer.
+        char (char): string of the integer.
+        other (char): string of the integer.
 
     Returns:
         (int): sum of the integer value of `x` and integer value of `y`.
     """
-    return int(x) + int(y)
+    return int(char) + int(other)
