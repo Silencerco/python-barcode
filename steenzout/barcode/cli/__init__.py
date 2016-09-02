7346b2146f8bcd8ac3c4ce58b3bb0db19fdf9c7b
Pedro Salgado <steenzout@ymail.com>
2016-09-02 11:17:07 -0600

    ability to handle input file and string.

--- steenzout/barcode/cli/__init__.py
+++ steenzout/barcode/cli/__init__.py
@@ -19,12 +19,13 @@ def cli():
 
 @cli.command()
 @click.option('-v', '--verbose', 'verbosity', count=True, help='Enables verbosity.')
+@click.option('-c', '--code', 'code', type=click.STRING)
+@click.option('-i', '--input', 'input', type=click.File('rb'))
 @click.option('-e', '--encoding', 'encoding', default='code39', type=click.Choice(barcode.encodings()))
 @click.option('-f', '--format', 'format', default='SVG', type=click.Choice(barcode.formats()))
 @click.option('-u', '--unit', 'unit', type=click.STRING)
-@click.argument('input', type=click.File('rb'))
 @click.argument('output', type=click.File('wb'))
-def generate(verbosity, encoding, format, unit, input, output):
+def generate(verbosity, code, input, encoding, format, unit, output):
     """Generates the bar code."""
     LOGGER.debug('generate(): %s, %s, %s, %s, %s, %s', verbosity, encoding, format, unit, input, output)
 
@@ -35,7 +36,12 @@ def generate(verbosity, encoding, format, unit, input, output):
         opts = dict(format=format)
         writer_instance = writer.ImageWriter()
 
-    barcode.generate(encoding, input.read(), writer_instance, output, opts)
+    if code is not None:
+        bar_code = code
+    else:
+        bar_code = input.read()
+
+    barcode.generate(encoding, bar_code, writer_instance, output, opts)
 
 
 @cli.command()
