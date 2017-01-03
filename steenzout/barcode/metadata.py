cb9b68511a2cf28c81a88b9378c8cf93da739763
Pedro Salgado <steenzout@ymail.com>
2017-01-02 21:34:27 -0700

    dropped Python 2.6 classifier.

--- steenzout/barcode/metadata.py
+++ steenzout/barcode/metadata.py
@@ -11,7 +11,6 @@ __classifiers__ = [
     'Operating System :: OS Independent',
     'Programming Language :: Python',
     'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.3',
